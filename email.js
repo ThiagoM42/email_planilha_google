57d822e00aff071a79e0bc24a839f35bfc4febb1
ThiagoM42 <tms.42@hotmail.com>
2020-02-11 14:35:14 -0300

    Add corpo da mensagem

--- email.js
+++ email.js
@@ -1,18 +1,28 @@
 function sendEmail() {
+
+  SpreadsheetApp.getActiveSpreadsheet().getSheetByName("dados").activate();
+
   var ss = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
   //pegando o último índice da linha
   var lr = ss.getLastRow();
+  
+  var templateText = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Template").getRange(1,1).getValue();
+  
+  var cotaEmail = MailApp.getRemainingDailyQuota();
+  Logger.log(cotaEmail)
+  
+  if(lr>cotaEmail){
+    Browser.msgBox("Cota de email diário ultrapassada "+(cotaEmail)+" tente novamente amanhã.");
+  }
   
   for(var i=2; i<=lr; i++){
     
     var currentEmail = ss.getRange(i, 5).getValue();
     var nome = ss.getRange(i, 3).getValue();
+    var assunto = "Setor de Estágio. Confirmação de orientação";
+    var messageBody  = templateText.replace("{name}", nome);
     //Logger.log(currentEmail)
-
-    var messageBody = '';
-
-    var templateText = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Template").getRange(1,1).getValue();
     
-    //MailApp.sendEmail(currentEmail, "Email de confirmação do FORM: ", "Olá "+nome)
+//    MailApp.sendEmail(currentEmail, assunto, messageBody)
   }
 }
