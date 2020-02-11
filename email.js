e6d89a8c2af8bd194620b35a14032b57921cc7d5
ThiagoM42 <tms.42@hotmail.com>
2020-02-11 15:29:52 -0300

    add função de confirmação na célula

--- email.js
+++ email.js
@@ -9,20 +9,38 @@ function sendEmail() {
   var templateText = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Template").getRange(1,1).getValue();
   
   var cotaEmail = MailApp.getRemainingDailyQuota();
-  Logger.log(cotaEmail)
   
   if(lr>cotaEmail){
     Browser.msgBox("Cota de email diário ultrapassada "+(cotaEmail)+" tente novamente amanhã.");
   }
+  else{ 
+    for(var i=2; i<=lr; i++){
+      
+      var currentEmail = ss.getRange(i, 5).getValue();
+      var nome = ss.getRange(i, 3).getValue();
+      var assunto = "Setor de Estágio. Confirmação de orientação";
+      var messageBody  = templateText.replace("{name}", nome);
+      var emailEnviado =ss.getRange(i, 6).getValue();
+      
+      if(emailEnviado){
+        MailApp.sendEmail(currentEmail, assunto, messageBody)
+      }
+    }//end for
+     Browser.msgBox("Email enviado de confirmação enviado para todos os professores");
+     emailConfirm();
+  } //end else
+}
+
+function emailConfirm(){
+  SpreadsheetApp.getActiveSpreadsheet().getSheetByName("dados").activate();
+  var ss = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  
+  var lr = ss.getLastRow();
   
   for(var i=2; i<=lr; i++){
-    
-    var currentEmail = ss.getRange(i, 5).getValue();
-    var nome = ss.getRange(i, 3).getValue();
-    var assunto = "Setor de Estágio. Confirmação de orientação";
-    var messageBody  = templateText.replace("{name}", nome);
-    //Logger.log(currentEmail)
-    
-//    MailApp.sendEmail(currentEmail, assunto, messageBody)
+    ss.getRange(i, 6).setValue("Sim");
   }
+  
 }
+
+
