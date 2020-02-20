4bd3d7465e28911d8f6adf68c624da52b468c540
ThiagoM42 <tms.42@hotmail.com>
2020-02-20 11:46:23 -0300

    Add condição para celulas com emails vazios

--- email.js
+++ email.js
@@ -1,3 +1,11 @@
+
+var formID = '1E214puIOqI8aTfWg1UkywwO4eXbdp_5xw6gNaVJROaE';
+var ssID = '1VcHtSwKJetp6JBD-Rw6FyZQ-NFHOwbIwAllpt5MlX18';
+
+var wsData = SpreadsheetApp.openById(ssID).getSheetByName("dados_professores");
+var form = FormApp.openById(formID);
+var item = form.getItemById(1409839857);
+
 function sendEmail() {
 
   SpreadsheetApp.getActiveSpreadsheet().getSheetByName("dados").activate();
@@ -6,18 +14,14 @@ function sendEmail() {
   //pegando o último índice da linha
   var lr = ss.getLastRow();
   
-  var templateText = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Template").getRange(1,1).getValue();
+  var templateText = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Template").getRange(1,1).getValue();   
   
-  var cotaEmail = MailApp.getRemainingDailyQuota();
-  Logger.log(cotaEmail);
   
-  if(lr>cotaEmail){
-    Browser.msgBox("Cota de email diário ultrapassada "+(cotaEmail)+" tente novamente amanhã.");
-  }
-  else{ 
+
     for(var i=2; i<=lr; i++){
       
       var currentEmail = ss.getRange(i, 10).getValue();
+      Logger.log(currentEmail);
       var nome = ss.getRange(i, 3).getValue();
       var nomeProfessor = ss.getRange(i, 9).getValue();
       var curso = ss.getRange(i, 7).getValue();
@@ -25,14 +29,15 @@ function sendEmail() {
       var messageBody  = templateText.replace("{name}", nome).replace("{nome_professor}", nomeProfessor).replace("{curso}", curso);
       
       var confirmado =ss.getRange(i, 15).getValue();
+      var email_ja_enviado = ss.getRange(i, 14).getValue();
       
-      if(!confirmado){
+      if(!confirmado && !email_ja_enviado && currentEmail){
         MailApp.sendEmail(currentEmail, assunto, messageBody)
       }
     }//end for
      Browser.msgBox("Email enviado de confirmação enviado para todos os professores");
      emailConfirm();
-  } //end else
+  
 }
 
 function emailConfirm(){
@@ -43,16 +48,28 @@ function emailConfirm(){
   var lr = ss.getLastRow();
   
   for(var i=2; i<=lr; i++){
+    var currentEmail = ss.getRange(i, 10).getValue();
+    
     var qtd_email = ss.getRange(i, 14).getValue();
     qtd_email = qtd_email + 1;
     
     var confirmado =ss.getRange(i, 15).getValue();
+      var email_ja_enviado = ss.getRange(i, 14).getValue();
       
-    if(!confirmado){
+      if(!confirmado && !email_ja_enviado && currentEmail){
       ss.getRange(i, 14).setValue(qtd_email).setBackground('green').setFontColor('white').setFontFamily('bold');
     }
   }
   
+}
+
+
+function addLista() {
+  
+  var values = wsData.getRange(1, 2, wsData.getLastRow(), 1).getValues();
+  values = values.filter(function(val){return val != ""})
+  item.asListItem().setChoiceValues(values);
+    
 }
 
 
